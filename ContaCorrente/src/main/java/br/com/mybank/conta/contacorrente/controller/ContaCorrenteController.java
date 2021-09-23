880579fe6604a5075af4a60a9a2ba7cd698223b3
adakopke <adakopke@gmail.com>
2021-09-23 11:53:04 -0300

    Criação do CRUD para contacorrente.

--- ContaCorrente/src/main/java/br/com/mybank/conta/contacorrente/controller/ContaCorrenteController.java
+++ ContaCorrente/src/main/java/br/com/mybank/conta/contacorrente/controller/ContaCorrenteController.java
@@ -4,8 +4,11 @@ package br.com.mybank.conta.contacorrente.controller;
 import br.com.mybank.conta.contacorrente.domain.ContaCorrente;
 import br.com.mybank.conta.contacorrente.service.ContaCorrenteService;
 import lombok.AllArgsConstructor;
+import org.json.JSONException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,23 +20,35 @@ public class ContaCorrenteController {
     private final ContaCorrenteService contaCorrenteService;
 
     @PostMapping("adicionar")
-    public ContaCorrente adicionarContaCorrente(@RequestBody ContaCorrente contaCorrente) {
-        return contaCorrenteService.adicionarContaCorrente(contaCorrente);
+    public ResponseEntity<?> adicionarContaCorrente(
+        @RequestBody ContaCorrente contaCorrente, @RequestHeader (value = "Authorization", required = true) String token) throws JSONException, IOException {
+        return contaCorrenteService.adicionarContaCorrente(contaCorrente, token);
     }
 
+    //TODO permitir apenas para administrador
     @GetMapping("listartodas")
     public List<ContaCorrente>  listarTodas() {
         return contaCorrenteService.listarTodas();
     }
 
+
+    //TODO restringir para que apenas os microserviços  consigam fazer essa consulta
     @GetMapping("listar/{id}")
-    public Optional<ContaCorrente> listarPorId(@PathVariable Integer id) {
-        return contaCorrenteService.listarPorId(id);
+    public Optional<ContaCorrente> listarPorIdUsuario(@PathVariable Integer id) {
+        return contaCorrenteService.listarPorIdUsuario(id);
     }
 
-    @PutMapping("atualizar/{id}")
-    public ContaCorrente atualizarContaCorrente(@PathVariable Integer id, @RequestBody ContaCorrente contaCorrente) {
-        return contaCorrenteService.atualizarContacorrente(id, contaCorrente);
+    //TODO permitir apenas para administrador
+    @PutMapping("atualizar")
+    public ResponseEntity<?> atualizarContaCorrente(
+            @RequestBody ContaCorrente contaCorrente, @RequestHeader (value = "Authorization", required = true) String token) throws JSONException, IOException {
+        return contaCorrenteService.atualizarContacorrente(contaCorrente, token);
+    }
+
+    @DeleteMapping("remover")
+    public ResponseEntity<?> removerContaCorrente(
+            @RequestHeader (value = "Authorization", required = true) String token) throws JSONException, IOException {
+        return contaCorrenteService.removerContaCorrente(token);
     }
 
 }
--- ContaCorrente/src/main/java/br/com/mybank/conta/contacorrente/service/ContaCorrenteService.java
+++ ContaCorrente/src/main/java/br/com/mybank/conta/contacorrente/service/ContaCorrenteService.java
@@ -2,9 +2,19 @@ package br.com.mybank.conta.contacorrente.service;
 
 import br.com.mybank.conta.contacorrente.domain.ContaCorrente;
 import br.com.mybank.conta.contacorrente.repository.ContaCorrenteRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.AllArgsConstructor;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,9 +24,15 @@ public class ContaCorrenteService {
 
     private final ContaCorrenteRepository contaCorrenteRepository;
 
-    public ContaCorrente adicionarContaCorrente(ContaCorrente contaCorrente) {
+    public ResponseEntity<?> adicionarContaCorrente(ContaCorrente contaCorrente, String token) throws JSONException, IOException {
 
-        return contaCorrenteRepository.save(contaCorrente);
+        JSONObject tokenJson = new JSONObject(jwtFilter(token));
+
+        if (!isValidBearerToken(token) || !contaCorrente.getIdUsuario().equals(tokenJson.get("id"))) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token expirado ou inválido");
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(contaCorrenteRepository.save(contaCorrente));
 
     }
 
@@ -25,20 +41,75 @@ public class ContaCorrenteService {
     }
 
 
-    public Optional<ContaCorrente> listarPorId(Integer id) {
+    public Optional<ContaCorrente> listarPorIdUsuario(Integer id)  {
+        return contaCorrenteRepository.findByIdUsuario(id);
 
-        return contaCorrenteRepository.findById(id);
 
     }
 
-    public ContaCorrente atualizarContacorrente(Integer id, ContaCorrente contaCorrente) {
+    public ResponseEntity<?> atualizarContacorrente(ContaCorrente contaCorrente, String token) throws JSONException, IOException {
 
-        if (listarPorId(id).isEmpty() || !listarPorId(id).get().getId().equals(id)) {
-            return null;
+        JSONObject tokenJson = new JSONObject(jwtFilter(token));
+
+        if (!isValidBearerToken(token) || !contaCorrente.getIdUsuario().equals(tokenJson.get("id"))) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token expirado ou inválido");
         }
 
-        contaCorrente.setId(id);
-        return contaCorrenteRepository.save(contaCorrente);
+        contaCorrente.setId(listarPorIdUsuario((Integer) tokenJson.get("id")).get().getId());
+        return ResponseEntity.status(HttpStatus.OK).body(contaCorrenteRepository.save(contaCorrente));
 
     }
+
+
+    public ResponseEntity<?> removerContaCorrente(String token) throws JSONException, IOException {
+
+        JSONObject tokenJson = new JSONObject(jwtFilter(token));
+        if (!isValidBearerToken(token)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token expirado ou inválido");
+        }
+
+        ContaCorrente contaCorrente = listarPorIdUsuario((Integer) tokenJson.get("id")).get();
+
+        if (!contaCorrente.isAtivo()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Conta já está desativada");
+        }
+
+        if (contaCorrente.getSaldoCorrente().compareTo(BigDecimal.valueOf(0)) > 0 ) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Conta corrente possui saldo");
+        }
+
+        if (contaCorrente.getSaldoEspecial().compareTo(BigDecimal.valueOf(0)) > 0 ) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Conta corrente está utilizando o especial");
+        }
+
+        contaCorrente.setAtivo(false);
+        return ResponseEntity.status(HttpStatus.OK).body(contaCorrenteRepository.save(contaCorrente));
+    }
+
+
+    private String jwtFilter(String token){
+        Base64.Decoder decoder = Base64.getDecoder();
+        token.replace("Bearer", " ");
+        String[] chunks = token.split("\\.");
+        String payload = new String(decoder.decode(chunks[1]));
+        return payload;
+    }
+
+
+    private boolean isValidBearerToken(String accessToken) throws IOException {
+        boolean isValid = false;
+        String token = accessToken.replace("Bearer ", "");
+        try {
+            long jwtExpiresAt = JWT.decode(token).getExpiresAt().getTime()/1000;
+            long difference = jwtExpiresAt - (System.currentTimeMillis()/1000);
+            if(difference >= 30){
+                isValid = true;
+            }
+        } catch (JWTDecodeException exception){
+            throw new IOException(exception.getMessage());
+        }
+        return isValid;
+    }
+
+
 }
